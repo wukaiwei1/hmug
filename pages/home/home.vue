96b3a7496ac23e8b3270993171650c5896b22dba
wukaiwei1 <2596862515@qq.com>
2022-09-05 11:38:17 +0800

    完成页面布局

---
+++ pages/home/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+  .topSearch {
+    width: 750rpx;
+    height: 175rpx;
+    background-color: #c00000;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+
+    .search_btn {
+      width: 550rpx;
+      height: 30px;
+      margin-top: 50px;
+      background-color: #fff;
+      border-radius: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+</style>
